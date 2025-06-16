3b8b76524afed9a935837ce21a919d7e6fff5a17
MAD <madalmanza@gmail.com>
2025-06-16 17:05:18 -0400

    changes add loger pino and jwt strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,26 @@ import { TransactionsModule } from './transactions/transactions.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { DepartmentsModule } from './departments/departments.module';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [AuthModule, EmployeesModule, ProductsModule, TransactionsModule, UsersModule,DepartmentsModule,
     ConfigModule.forRoot({
       isGlobal: true,
-    })
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            ignore: 'pid,hostname',
+            translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
+            singleLine: true,
+          },
+        },
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,38 +1,37 @@
 import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { Request } from 'express';
-import { ExecutionContext } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard {
-  constructor(
-    private readonly jwtService: JwtService,
-  ) {}
+export class JwtAuthGuard extends AuthGuard('jwt') { }
+// {
+//   constructor(
+//     private readonly jwtService: JwtService,
+//   ) {}
 
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    
-    try {
-      const token = this.extractTokenFromHeader(request);
-      if (!token) {
-        return false;
-      }
-      const payload = await this.jwtService.verifyAsync(token);
-      request['user'] = payload;
-      return true;
-    } catch {
-      return false;
-    }
-  }
+//   async canActivate(context: ExecutionContext): Promise<boolean> {
+//     const request = context.switchToHttp().getRequest();
 
-  private extractTokenFromHeader(request: Request): string | undefined {
-    const authHeader = request.headers.authorization;
-    if (authHeader) {
-      const [type, token] = authHeader.split(' ');
-      if (type === 'Bearer' && token) {
-        return token;
-      }
-    }
-    return undefined;
-  }
-}
+//     try {
+//       const token = this.extractTokenFromHeader(request);
+//       if (!token) {
+//         return false;
+//       }
+//       const payload = await this.jwtService.verifyAsync(token);
+//       request['user'] = payload;
+//       return false;
+//     } catch {
+//       return false;
+//     }
+//   }
+
+//   private extractTokenFromHeader(request: Request): string | undefined {
+//     const authHeader = request.headers.authorization;
+//     if (authHeader) {
+//       const [type, token] = authHeader.split(' ');
+//       if (type === 'Bearer' && token) {
+//         return token;
+//       }
+//     }
+//     return undefined;
+//   }
+// }
